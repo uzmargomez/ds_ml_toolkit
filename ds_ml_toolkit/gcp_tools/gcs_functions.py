089c89732c4609c38510fce87688143e38a91860
Uzmar Gomez <uzmar.gomez@ciencias.unam.mx>
2021-09-16 10:18:27 -0500

    add new function

--- ds_ml_toolkit/gcp_tools/gcs_functions.py
+++ ds_ml_toolkit/gcp_tools/gcs_functions.py
@@ -12,3 +12,15 @@ def look_for_file(bucket_name,file_name):
     bucket = storage_client.bucket(bucket_name)
     stats = storage.Blob(bucket=bucket, name=file_name).exists(storage_client)
     return stats
+
+def parse_gcs_uri(uri_string):
+    prefix = "gs://"
+    if uri_string.find(prefix) != 0:
+        raise Exception("The uri is not a gcs path")
+    uri_string = uri_string.replace(prefix, "")
+    split_path = uri_string.split("/")
+    bucket = split_path[0]
+    file_name = split_path[-1]
+    uri_string = uri_string.replace(f"{bucket}/", "")
+    directory = uri_string.replace(file_name, "")
+    return {"bucket": bucket, "directory": directory, "file_name": file_name, "uri":uri_string}
